23ab02f9d48f5163c585c7b33f977c6f911c90eb
Pitter911 <pedrogarcia8323@mgail.com>
2025-02-23 15:35:53 -0600

    Added navbar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ResgisterComponent } from './resgister/resgister.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+     path: 'login',
+     component: LoginComponent   
+    },
+    {
+        path: 'register',
+        component: ResgisterComponent
+    }
+];
