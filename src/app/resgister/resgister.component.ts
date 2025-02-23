ff709f1104492b38be1c261e86c601fa9684385e
Joshi <joshi.jc2032@gmail.com>
2025-02-23 00:12:53 -0600

    start-project

---
+++ src/app/resgister/resgister.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resgister',
+  imports: [],
+  templateUrl: './resgister.component.html',
+  styleUrl: './resgister.component.scss'
+})
+export class ResgisterComponent {
+
+}
